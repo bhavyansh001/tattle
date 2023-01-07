5cd24c322409dc415bb6f0732c0e24ddc6149da8
Bhavyansh <bhavyansh001@gmail.com>
2023-01-07 16:58:27 +0530

    Fixed known bugs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,23 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def new
-    @article = Article.new
+    @article = current_user.articles.build
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
+    @article.user_id = current_user.id
     if @article.save
-      redirect_to @article
+      redirect_to @article, notice: 'Article was successfully created.'
     else
-      render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity, notice: 'Article could not be created'
     end
   end
 
@@ -24,20 +28,30 @@ class ArticlesController < ApplicationController
   end
   
   def update
+    
     if @article.update(article_params)
-      redirect_to @article
+      redirect_to @article,  notice: 'Article was successfully updated.'
     else
-      render :edit, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity, notice: 'Article could not be updated.'
     end
   end
 
   def destroy
-    @article.destroy
+    if @article.destroy
+      redirect_to articles_path, status: :see_other, notice: 'Article was successfully deleted.'
+    else
+      render @article, notice: 'Article could not be deleted.'
+    end
   end
 
   private
+  def correct_user
+    @user = current_user.articles.find_by(id: params[:id])
+    redirect_to articles_path, notice: 'Not authorized to edit this article!' if @user.nil?
+  end
   def set_article
     @article = Article.find(params[:id])
+    @article_user = @article.user.email.split('@')[0]
   end
   def article_params
     params.require(:article).permit(:title, :body)
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
     has_rich_text :body
+    belongs_to :user
+
 end
