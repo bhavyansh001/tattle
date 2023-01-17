34c5ca7edba7d16969adc0ea3987ca3efa3f2583
Bhavyansh <bhavyansh001@gmail.com>
2023-01-17 17:46:08 +0530

    Stimulus feature added for scoll and clearing the form

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,17 @@
+import {Controller} from "@hotwired/stimulus"
+
+export default class extends Controller{
+    connect(){
+        console.log("conneted!");
+        const messages = document.getElementById("messages");
+        messages.addEventListener("DOMNodeInserted", this.resetScroll);
+        this.resetScroll(messages);
+
+    }
+    disconnect(){
+        console.log("disconnected!");
+    }
+    resetScroll(){
+        messages.scrollTop = messages.scrollHeight - messages.clientHeight
+    }
+}
