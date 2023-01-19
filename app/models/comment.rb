682a1edbbd5b658cd86ff427168f2ec9a593eabc
Bhavyansh <bhavyansh001@gmail.com>
2023-01-19 20:56:14 +0530

    Caching enabled

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   has_rich_text :body
   validates :name, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-  belongs_to :article
+  belongs_to :article, touch: true
   belongs_to :user
 end
