305a34c4bd23ca5d05b0416c3c180f77c32b4b7e
Bhavyansh <bhavyansh001@gmail.com>
2024-06-16 10:34:43 +0530

    Update IP for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '54.160.161.65', user: 'deploy', roles: %w{app db web}
+server '35.172.211.121', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
